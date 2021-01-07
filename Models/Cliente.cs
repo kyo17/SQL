7792861443b7f92d9f771e0216a89cff0243fc81
Jonathan <57236419+kyo17@users.noreply.github.com>
2021-01-06 19:00:56 -0600

    Add files via upload
    Commit v2

---
+++ Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Models
+{
+    public class Cliente
+    {
+        public Cliente()
+        {
+        }
+        [Key]
+        public Guid idCliente { get; set; }
+        [Required(ErrorMessage = "Este campo es requerido"), MaxLength(40)]
+        public string nombre { get; set; }
+        [Required(ErrorMessage = "Este campo es requerido"), MaxLength(40), EmailAddress]
+        public string email { get; set; }
+    }
+}
